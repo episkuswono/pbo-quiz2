3482f3de0034406a3850188e14a64f75dad12bb3
Epis Kuswono <episkuswono@gmail.com>
2018-11-29 18:36:24 +0700

    ada perpindahan file karena terjadi kesalahan pada package

---
+++ test/com/epis/quis2/Item.java
@@ -0,0 +1,67 @@
+package com.epis.quis2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Epis Kuswono
+ */
+public class Item {
+    private String nama;
+    private float harga;
+    private int jumlah;
+
+    public Item(String nama, int jumlah) {
+        this.nama = nama;
+        this.jumlah = jumlah;
+    }
+
+    public Item(String nama, float harga, int jumlah) {
+        this.nama = nama;
+        this.harga = harga;
+        this.jumlah = jumlah;
+        switch (nama) {
+            case "Kopi" : this.harga = 10000;
+            break;
+            case "Gula" : this.harga = 30000;
+            break;
+            case "Susu" : this.harga = 20000;
+            break;
+        }
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public float getHarga() {
+        return harga;
+    }
+
+    public void setHarga(float harga) {
+        this.harga = harga;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+    
+    public float getTotal(){
+        return this.harga * this.jumlah;
+    }
+    public String toString(){
+        return this.nama;
+    }
+}
