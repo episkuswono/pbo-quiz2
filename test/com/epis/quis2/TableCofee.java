95fabb31ce8a2bebda6c5699d70736f42ee8f9c8
Epis Kuswono <episkuswono@gmail.com>
2018-11-29 18:50:31 +0700

    dan buat kelas tablecoffee untuk mengextend defaultTableModel dan buat satu variable kolom pada String array untuk menyimpan nama kolom

---
+++ test/com/epis/quis2/TableCofee.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.epis.quis2;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Epis Kuswono
+ */
+public class TableCofee extends DefaultTableModel{
+    private String[] kolom;
+}
